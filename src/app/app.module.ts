6c930a7ec9f122273b95519dd101830334b2545e
Daniel H <daguiheso@gmail.com>
2020-06-29 13:32:44 -0500

    use components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './components/app/app-routing.module';
 import { AppComponent } from './components/app/app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { TemplatesComponent } from './components/templates/templates.component';
+import { EnumAndCastingComponent } from './components/enum-and-casting/enum-and-casting.component';
+import { AttributesEventsAndNgModelComponent } from './components/attributes-events-and-ng-model/attributes-events-and-ng-model.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TemplatesComponent,
+    EnumAndCastingComponent,
+    AttributesEventsAndNgModelComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
